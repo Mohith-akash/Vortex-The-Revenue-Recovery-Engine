3bf8502d58b096c2e054e1232c1e6e8b62244e4c
Mohith-akash <mohithakash013@gmail.com>
2026-01-17 11:00:00 +0530

    dbt project for analytics layer

---
+++ vortex_analytics/models/marts/gold_user_sales.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='table',
+    file_format='delta'
+) }}
+
+with silver_data as (
+    -- We read from the source we just defined in sources.yml
+    select * from {{ source('vortex_source', 'vortex_silver') }}
+),
+
+aggregated_data as (
+    -- This is the business logic: Who buys the most?
+    select
+        user_archetype,
+        risk_level,
+        count(event_id) as total_transactions,
+        sum(cart_total) as total_revenue,
+        avg(cart_total) as avg_transaction_value
+    from silver_data
+    group by 1, 2
+)
+
+select * from aggregated_data
+order by total_revenue desc
